4a0a13ffa47b4172e156662ac6d88b972710dd09
Dibyaranjan Gouda <DGouda@ESTUATE.COM>
2017-06-09 17:25:23 +0530

    cummit 09/06

--- demo25/index2.js
+++ demo25/index2.js
@@ -8,50 +8,48 @@ app.factory('Factory', ['$http',function($http){
     }
 ]);
 app.controller('Mcrt',['$scope', 'Factory',function ($scope,Factory){
-	$scope.nval=true;
-	$scope.side=false;
+	$scope.nval=false;
+	$scope.slideclass='slideroff';
+	$scope.dropmenu='submenu';
+	$scope.slidedrop=false;
+	$scope.dropvar=false;
         Factory.data().then(
             function resolved (response) {
                 $scope.data = response.data;
-				//console.log($scope.data[1].sub);
             },
             function rejected (response) {
                 alert(response.status + ': ' + response.statusText);
             }
         );
-		$scope.show=function(x){
-		
-			$scope.tmp=$scope.data[x];
-			$scope.clas=$scope.data[x].clas;
-			$scope.val=$scope.tmp.clas;
-				
-		};	
-		$scope.hoverIn = function(){
-        $scope.hoverEdit = true;
-		};
-		$scope.hoverOut = function(){
-			$scope.hoverEdit = false;
-		};
+	
 		$scope.slide=function()
-								{   debugger;
-									var myEl = angular.element( document.querySelector( '.nav1' ) );
-									var navslider = angular.element( document.querySelector( '.sidenav li' ) );
+								{   
 									var dashmove = angular.element( document.querySelector( '.dash' ) );
-									if($scope.nval==false)
+									if($scope.nval==true)
 										{	
-											myEl.removeClass('slider');
-											navslider.css('width','70px');
+											$scope.slideclass='slideroff';
 											dashmove.css('marginLeft','80px');
-											$scope.nval=true;
+											$scope.nval=false;
+											$scope.dropmenu='submenu';
+											$scope.slidedrop=false;
 										}
 									else
-										{
-										myEl.addClass('slider');	
-										navslider.css('width','250px');	
-										dashmove.css('marginLeft','260px');
-										$scope.nval=false;																								
+										{	
+											$scope.slideclass='slideron';
+											dashmove.css('marginLeft','260px');
+											$scope.nval=true;
+											$scope.dropmenu='submenudrop';
+											$scope.slidedrop=true;																	
 										}
 								};
-		
-    }
+		$scope.dropit=function(x)
+		{
+				$scope.xid=x;
+		};
+
+
+
+
+		}
+    
 ]);
